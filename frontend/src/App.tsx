50fdfb74499e21434339e96b01c4cf0bf00c360a
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-03-27 22:54:58 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Appbar from "./components/Appbar"
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <div className="bg-gray-950 h-screen text-white">
+      <Appbar/>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Appbar.tsx
@@ -0,0 +1,23 @@
+
+function Appbar() {
+  return (
+    <div className="py-7 shadow-white shadow-sm  w-screen">
+      <div className="flex justify-between mx-10">
+        <div className="text-2xl font-bold">
+            Welcome User
+        </div>
+        <div className="flex space-x-10 text-lg font-semibold">
+            <div className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out">About</div>
+            <div className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out">Speaker</div>
+            <div className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out">Topics</div>
+            <div className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out">Book Online</div>
+            <div className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out">Login</div>
+            <div className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out" >Register now</div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Appbar
