bc5563db97d5b41d2a340a9e99bac8467a3e9b6a
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-04-01 22:46:04 +0530

    shop logic update

--- frontend/src/pages/Shop.tsx
+++ frontend/src/pages/Shop.tsx
@@ -1,15 +1,32 @@
-import { useNavigate } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import URI from "../config";
 
 function Shop() {
-    const Navigate = useNavigate()
-    if(localStorage.getItem('token') == " "){
-        Navigate('/signin')
+  const Navigate = useNavigate();
+
+  useEffect(() => {
+    async function call() {
+      const token = localStorage.getItem("token");
+      const res = await axios.get(`${URI}/api/v1/user/auth`, {
+        headers: {
+          Authorization: token,
+        },
+      });
+      // if(!token){
+      //   Navigate('/')
+      // }
+      console.log(res.data.user);
+      if(res.data.user.verified == false){
+        Navigate('/otp-verify')
+      }
     }
-  return (
-    <div>
-      Shopping page
-    </div>
-  )
+    call();
+  }, []);
+
+  
+  return(  <div>Shopping page</div>);
 }
 
-export default Shop
+export default Shop;
