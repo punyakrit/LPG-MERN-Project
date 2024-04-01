ef48b52ac2a273cd5a2239d305856ae79dbdfbaf
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-04-01 11:41:16 +0530

    app bar logic

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,10 +1,42 @@
 // import About from "./About";
 
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import URI from "../config";
+
+interface UserData {
+  username?: string;
+}
 
 function Appbar() {
+  const [data, setData] = useState<UserData>({});
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
-  const Navigate = useNavigate()
+  useEffect(() => {
+    async function call() {
+      try {
+        const token = localStorage.getItem("token");
+        const res = await axios.get(`${URI}/api/v1/user/auth`, {
+          headers: {
+            Authorization: token,
+          },
+        });
+        setData(res.data.user);
+        setIsLoggedIn(true);
+      } catch {
+        setIsLoggedIn(false);
+        console.log("error");
+      }
+    }
+    call();
+  }, []);
+  const handleLogout = () => {
+    localStorage.removeItem("token"); // Remove token from localStorage
+    setIsLoggedIn(false); // Set login status to false
+    Navigate("/signin"); // Navigate to login page
+  };
+  const Navigate = useNavigate();
 
   const handleScrollToAbout = () => {
     const aboutComponent = document.getElementById("about");
@@ -29,7 +61,7 @@ function Appbar() {
   return (
     <div className="py-7 shadow-white shadow-sm   ">
       <div className="flex justify-between mx-10">
-        <div className="text-2xl font-bold">Welcome User</div>
+        <div className="text-2xl font-bold">{isLoggedIn ? `Welcome ${data.username}` : `Welcome User`} </div>
         <div className="flex space-x-10 text-lg font-semibold">
           <div
             className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out"
@@ -37,24 +69,34 @@ function Appbar() {
           >
             About
           </div>
-          <div className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out"
-          onClick={handleScrollTSpaker}>
+          <div
+            className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out"
+            onClick={handleScrollTSpaker}
+          >
             Speaker
           </div>
-          <div className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out"
-          onClick={handleScrollToNewsLetter}>
+          <div
+            className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out"
+            onClick={handleScrollToNewsLetter}
+          >
             News Letter
           </div>
-          <div className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out">
+          <div
+            className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out"
+            onClick={() => Navigate("/shop")}
+          >
             Book Online
           </div>
-          <div className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out"
-            onClick={()=> Navigate('/signin')}
+          <div
+            className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out"
+            onClick={handleLogout}
           >
-            Login
+            {isLoggedIn ? `Logout` : `Login`}
           </div>
-          <div className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out"
-          onClick={()=> Navigate('/signup')}
+
+          <div
+            className="cursor-pointer text-white/50 hover:text-white transition duration-500 ease-in-out"
+            onClick={() => Navigate("/signup")}
           >
             Register now
           </div>
--- frontend/src/pages/Shop.tsx
+++ frontend/src/pages/Shop.tsx
@@ -1,5 +1,10 @@
+import { useNavigate } from "react-router-dom"
 
 function Shop() {
+    const Navigate = useNavigate()
+    if(localStorage.getItem('token') == " "){
+        Navigate('/signin')
+    }
   return (
     <div>
       Shopping page
