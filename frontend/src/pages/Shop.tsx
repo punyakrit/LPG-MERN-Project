0f522db973a1b4c8d9d3994dc5e60fb2e52889b6
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-04-01 22:46:57 +0530

    update

--- frontend/src/pages/Shop.tsx
+++ frontend/src/pages/Shop.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect,  } from "react";
 import { useNavigate } from "react-router-dom";
 import URI from "../config";
 
